55de2cf90a78889d2cf2651dc685e05b8491ef78
Natnael Endale <n87294578@gmail.com>
2026-02-05 11:27:52 -0800

    divide app.js in to two files

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ dotenv.config({ path: './config.env' });
 const mongoose = require('mongoose');
 const app = require('./app');
 
+const port = process.env.PORT || 3000;
+
 const dbConnectionString = process.env.DB_CONNECTION_STRING.replace(
   '<db_password>',
-  process.env.DB_PASSWORD
+  process.env.DB_PASSWORD,
 );
 
 mongoose
   .connect(dbConnectionString)
   .then(() => {
-    console.log('Database connected succesfully.');
+    console.log('Database connected successfully.');
+    app.listen(port, () => console.log(`Server running on port ${port}`));
   })
   .catch(() => {
     console.log('Database not connected.');
   });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
