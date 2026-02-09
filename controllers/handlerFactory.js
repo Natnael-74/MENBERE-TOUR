bf202d93d46b751f30330c5f442f05068a1ca0e1
Natnael Endale <n87294578@gmail.com>
2026-02-09 11:53:55 -0800

    arrange the creating

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -37,11 +37,11 @@ export const getOne = (Model, popOptions) =>
     res.status(200).json({ status: 'success', data: { doc } });
   });
 
-// export const createOne = (Model) =>
-//   catchAsync(async (req, res, next) => {
-//     const doc = await Model.create(req.body);
-//     res.status(201).json({ status: 'success', data: { doc } });
-//   });
+export const createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+    res.status(201).json({ status: 'success', data: { doc } });
+  });
 
 // export const updateOne = (Model) =>
 //   catchAsync(async (req, res, next) => {
