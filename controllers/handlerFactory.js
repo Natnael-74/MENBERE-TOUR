f295f8a1790b40010f2b060df17857a5e577fc07
Natnael Endale <n87294578@gmail.com>
2026-02-09 11:53:32 -0800

    arrange the readings

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -2,40 +2,40 @@ import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
 import APIFeatures from '../utils/APIFeatures.js';
 
-// export const getAll = (Model) =>
-//   catchAsync(async (req, res, next) => {
-//     // Nested Routes
-//     let filter = {};
-//     if (req.params.tourId) filter = { tour: req.params.tourId };
+export const getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // Nested Routes
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
 
-//     const features = new APIFeatures(Model.find(filter), req.query)
-//       .filter()
-//       .search()
-//       .sort()
-//       .limitFields();
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .search()
+      .sort()
+      .limitFields();
 
-//     await features.paginate();
+    await features.paginate();
 
-//     const docs = await features.query;
+    const docs = await features.query;
 
-//     res.status(200).json({
-//       status: 'success',
-//       results: docs.length ? docs.length : 0,
-//       pagination: features.pagination,
-//       data: {
-//         docs,
-//       },
-//     });
-//   });
+    res.status(200).json({
+      status: 'success',
+      results: docs.length ? docs.length : 0,
+      pagination: features.pagination,
+      data: {
+        docs,
+      },
+    });
+  });
 
-// export const getOne = (Model, popOptions) =>
-//   catchAsync(async (req, res, next) => {
-//     let query = Model.findById(req.params.id);
-//     if (popOptions) query = query.populate(popOptions);
-//     const doc = await query;
-//     if (!doc) return next(new AppError('No document found with that ID.', 404));
-//     res.status(200).json({ status: 'success', data: { doc } });
-//   });
+export const getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+    if (!doc) return next(new AppError('No document found with that ID.', 404));
+    res.status(200).json({ status: 'success', data: { doc } });
+  });
 
 // export const createOne = (Model) =>
 //   catchAsync(async (req, res, next) => {
