c9fb3b180f762d0323776cdb838adbc83f6c46a7
Natnael Endale <n87294578@gmail.com>
2026-02-09 11:54:35 -0800

    add the delete

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -53,9 +53,9 @@ export const updateOne = (Model) =>
     res.status(200).json({ status: 'success', data: { doc } });
   });
 
-// export const deleteOne = (Model) =>
-//   catchAsync(async (req, res, next) => {
-//     const doc = await Model.findByIdAndDelete(req.params.id);
-//     if (!doc) return next(new AppError('No document found with that ID.', 404));
-//     res.status(204).json({ status: 'success', data: null });
-//   });
+export const deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+    if (!doc) return next(new AppError('No document found with that ID.', 404));
+    res.status(204).json({ status: 'success', data: null });
+  });
