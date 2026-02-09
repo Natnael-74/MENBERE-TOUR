89cf667f6b9fed08113dab9a775fe6566ec00c0d
Natnael Endale <n87294578@gmail.com>
2026-02-09 11:54:19 -0800

    add the updating

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -43,15 +43,15 @@ export const createOne = (Model) =>
     res.status(201).json({ status: 'success', data: { doc } });
   });
 
-// export const updateOne = (Model) =>
-//   catchAsync(async (req, res, next) => {
-//     const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
-//       new: true,
-//       runValidators: true,
-//     });
-//     if (!doc) return next(new AppError('No document found with that ID.', 404));
-//     res.status(200).json({ status: 'success', data: { doc } });
-//   });
+export const updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!doc) return next(new AppError('No document found with that ID.', 404));
+    res.status(200).json({ status: 'success', data: { doc } });
+  });
 
 // export const deleteOne = (Model) =>
 //   catchAsync(async (req, res, next) => {
