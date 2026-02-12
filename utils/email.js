6ced907dfb2bea174eadc4edd5e58d0b42c69aaf
Natnael Endale <n87294578@gmail.com>
2026-02-12 10:24:03 -0800

    create a transporter for both production and development

--- utils/email.js
+++ utils/email.js
@@ -3,3 +3,65 @@ import pug from 'pug';
 import htmlToText from 'html-to-text';
 
 import __dirname from './directory.js';
+
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Natnael Endale <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send the actual email
+//   async send(template, subject) {
+//     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+//       firstName: this.firstName,
+//       url: this.url,
+//       subject,
+//     });
+
+//     const mailOptions = {
+//       from: this.from,
+//       to: this.to,
+//       subject,
+//       html,
+//       text: htmlToText.fromString(html),
+//     };
+
+//     await this.newTransport().sendMail(mailOptions);
+//   }
+
+//   async sendWelcome() {
+//     await this.send('welcome', 'Welcome to the Menbere Family!');
+//   }
+
+//   async sendPasswordReset() {
+//     await this.send(
+//       'passwordReset',
+//       'Your password reset token (valid for only 10 minutes)',
+//     );
+//   }
+// }
+
+//export default Email;
