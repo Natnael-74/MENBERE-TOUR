aa2a750af2e62ee7d14b88576b4c7a4707019003
Natnael Endale <n87294578@gmail.com>
2026-02-21 02:37:39 -0800

    Delete user routes from userRoute.js
    Removed user route definitions and exports.

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,1 @@
-const express = require('express');
-const userController = require('../controller/userController');
 
-const router = express.Router();
-
-router
-  .route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
-router
-  .route('/id')
-  .delete(userController.deleteUser)
-  .patch(userController.updateUser);
-
-module.exports = router;
